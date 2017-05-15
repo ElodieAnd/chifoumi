37958b52a0fedc5f82d5a8dc461bf0cb4ab0f830
Ophelie Decuyper <ophelie.decuyper@orange.fr>
2017-05-15 17:44:22 +0200

    Ajout des logs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <ctime>
 #include <string>
+#include <glog/logging.h>
 #include "Chifoumi.hpp"
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
+	
 	std::string util;
 	
 	std::cout << "**** Chifoumi **** \n \n";
@@ -14,4 +18,17 @@ int main(int argc, char *argv[])
 	
 	std::cout << "\n" << chifoumi.play(util) << "(" << chifoumi.getOrdinateur() << "," << util << ") \n";
 	
+	 //std::fstream fichier("logfile.txt", std::ios::in | std::ios::out | std::ios::ate);
+	 //if(fichier){
+	//	std::string ligne;
+	//	while(getline(fichier, ligne)){ // tant que l'on peut mettre la ligne dans "contenu"
+		
+	//		std::cout << ligne << std::endl; // on l'affiche
+	//	}
+		
+	//	fichier << "Log file created at :" << "\n" << std::endl; 
+		
+	//} 
 }
+
+
